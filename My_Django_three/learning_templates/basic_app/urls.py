8e843065fd61ecb864121ecbf7284acebcf808e3
unknown <Administratr@E-VUX13019X.tyreso.se>
2021-06-03 14:15:04 +0200

    learning_templates

---
+++ My_Django_three/learning_templates/basic_app/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from basic_app import views
+
+#TENPLATE TAGGING
+
+app_name = 'basic_app'
+
+
+urlpatterns = [
+    url(r'^relative/$', views.relative, name='relative'),
+    url(r'^other/$',views.other, name='other'),
+]
